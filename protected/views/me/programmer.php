0d516cdc395da266edce63b85b70be05337bdf60
Mark Bryk <bryk@cooper.edu>
2012-12-05 17:49:38 -0500

    done building the grammer page

--- protected/views/me/programmer.php
+++ protected/views/me/programmer.php
@@ -6,28 +6,133 @@
                 <li class=""><a href="#AB" data-toggle="tab">Aleph Beta</a></li>
                 <li class=""><a href="#SLAP" data-toggle="tab">SLAP</a></li>
                 <li class=""><a href="#ME" data-toggle="tab">Mark 2012</a></li>
+                <li class=""><a href="#SS" data-toggle="tab">SoloSports</a></li>
                 <li class=""><a href="#DSA" data-toggle="tab">Data Structures and Algorithms</a></li>
                 <li class=""><a href="#GNG" data-toggle="tab">Give n' Grow</a></li>
                 <li class=""><a href="#BB" data-toggle="tab">Brick Breaker</a></li>
               </ul>
               <div class="tab-content">
                 <div class="tab-pane active" id="PB">
-                    <p><a href="http://www.pinbooster.com">Check it out</a></p>
+                    <a href="http://betabeat.com/2012/06/pinbooster-pinterest-advertisers-brands-sponsored-pins/">
+                        <div class="link-card link-card-left">
+                            <img src="http://links.services.disqus.com/api/click?format=go&key=cfdfcf52dffd0a702a61bad27507376d&loc=http%3A%2F%2Fbetabeat.com%2F2012%2F06%2Fpinbooster-pinterest-advertisers-brands-sponsored-pins%2F&subId=673609&v=1&libid=1354741195428&out=http%3A%2F%2Fnyobetabeat.files.wordpress.com%2F2012%2F06%2Fpinbooster.jpg&ref=http%3A%2F%2Fwww.google.com%2Furl%3Fsa%3Dt%26rct%3Dj%26q%3D%26esrc%3Ds%26source%3Dweb%26cd%3D4%26ved%3D0CEUQFjAD%26url%3Dhttp%253A%252F%252Fbetabeat.com%252F2012%252F06%252Fpinbooster-pinterest-advertisers-brands-sponsored-pins%252F%26ei%3DrbW_UMGmCJOP0QGk8IHoCQ%26usg%3DAFQjCNHrasXKZEnMyqE-S1LMuY-dbPrDsA%26sig2%3DXYh1YJuBd4871ZQIhn2baw%26cad%3Drja&title=Pinbooster%20Brings%20Sponsored%20Pins%20to%20Pinterest%20%7C%20Betabeat&txt=&jsonp=vglnk_jsonp_13547414570473" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Pinbooster Brings Sponsored Pins to Pinterest</h3>
+                            <p>Pinbooster cofounder Dave Weinberg has been working feverishly to get a platform for sponsoring pins on Pinterest launched before any competitors do.</p>
+                            <p>It would appear that for now, Pinbooster has the market for sponsored pins cornered. The site, which will allow advertisers to commission pins from power users, officially launched today.</p>
+                        </div>
+                    </a>
+                    
+                    <a href="http://techcrunch.com/2012/10/26/pinbooster-launches-its-pay-to-pin-ad-platform-for-pinterest/">
+                        <div class="link-card link-card-left">
+                            <img src="http://tctechcrunch2011.files.wordpress.com/2012/10/compassion-pin.jpg" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Pinbooster Launches Its Pay-To-Pin Ad Platform For Pinterest</h3>
+                            <p>This week, Pinterest cracked the top 50 most trafficked websites in the U.S. with its now 25.3 million stateside visitors, but the social photo-sharing site has yet to determine how it will monetize all those pins. </p>
+                            <p>However, that hasn’t stopped other companies from using Pinterest’s growing popularity to generate revenue for themselves. Case in point: today, a new startup called Pinbooster is officially opening its doors, becoming the first ad platform which pays users to re-pin ad messages from brands.</p>
+                        </div>
+                    </a>
+                    
+                    <a href="http://www.pinbooster.com/">
+                        <div class="link-card link-card-left">
+                            <img src="https://pinbooster.com/imgs/images/get-paid1.jpg" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Connecting Advertisers with Pinterest Users</h3>
+                            <p>Pinbooster is a platform to help Pinners monetize their following, and advertisers of all sizes to reach millions of engaged users.</p>
+                        </div>
+                    </a>
+                        
+                    
+                    
+                    <!--<p><a href="http://www.pinbooster.com">Check it out</a></p>-->
                 </div>
                 <div class="tab-pane" id="AB">
-                  <p><a href="http://www.alephbeta.org">Check it out</a></p>
+                    <a href="http://dev2.alephbeta.org">
+                        <div class="link-card link-card-left">
+                            <img src="http://dev2.alephbeta.org/themes/new/img/donate/dimage.jpg" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Reimagining Jewish Education</h3>
+                            <p>At Aleph Beta we are re-imagining Jewish education. By gathering the courses of master Judaic scholars with expertise in the fields of Jewish text, Jewish history and Jewish values, Aleph Beta has the ability to deliver a rich Jewish education, at a college level, to any student, anywhere, around the world.</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="tab-pane" id="SLAP">
-                  <p><a href="http://www.slapcenter.com">Check it out</a></p>
+                    <a href="http://www.slapcenter.com/">
+                        <div class="link-card link-card-left">
+                            <img src="http://www.slapcenter.com/grow-your-business/images/logo.png"/>
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>My Slap</h3>
+                            <p>Built a 20-page form for new registrations, looking for a SLAP.</p>
+                            <p>A SLAP (Silver Lining Action Plan) is a way for small business owners to increase their revenues while cutting their costs.</p>
+                        </div>
+                    </a>
                 </div>
                 <div class="tab-pane" id="ME">
-                  <p><a href="http://www.markbryk.in">Check it out</a></p>
+                    <a href="http://www.markbryk.in/">
+                        <div class="link-card link-card-left">
+                            <img src="http://www.markbryk.in/themes/new/images/sketches/brykachu1.png" height="130" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>This Site</h3>
+                            <p>:).</p>
+                            <p>I wrote it.</p>
+                            <p>All of it.</p>
+                        </div>
+                    </a>
                 </div>
-                <div class="tab-pane" id="GNG">
-                  <p><a href="http://dap.cooper.edu/doku.php?id=start:classes:principlesofdesign:giving_tree:start">Check it out</a></p>
+                  <div class="tab-pane" id="SS">
+                    <a href="http://solosports.markbryk.in/">
+                        <div class="link-card link-card-left">
+                            <img src="http://solosports.markbryk.in/themes/first/css/imgs/skier1.png" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Bringing the World Together</h3>
+                            <p>SoloSports is a platform to help Athletes in Sports which are simply better played in Groups. Just fill out a quick and innovatively visual survey of your talent, and see who the algorithm spits out!</p>
+                        </div>
+                    </a>
+                </div>                  
+                <div class="tab-pane" id="DSA">
+                    <a href="http://www.github.com/mbryk/DSA-2-Work">
+                        <div class="link-card link-card-left">
+                            <img src="http://www.xda-developers.com/wp-content/uploads/2012/09/github-round1.png?f39ce1" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Github</h3>
+                            <p>All of my code is on Github.</p>
+                            <p>Including Hash Tables, Binary Heaps, Djikstra's Algorithm, and Dynamic String Merging. </p>
+                            <p>Woo-hoo!</p>
+                        </div>
+                    </a>
                 </div>
+                  <div class="tab-pane" id="GNG">
+                      <a href="http://dap.cooper.edu/doku.php?id=start:classes:principlesofdesign:giving_tree:start">
+                        <div class="link-card link-card-left">
+                            <img src="http://dap.cooper.edu/lib/exe/fetch.php?media=start:classes:principlesofdesign:giving_tree:wikimainimage05.png" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>Give and Grow Android App</h3>
+                            <p>Used augmented reality on smart phones to create an interactive giving experience to incentivize and interest potential donors while creating a network of givers and helping the Cooper Union climb out of debt.</p>
+                            <p>All of the code is on my Github page.</p>
+                        </div>
+                      </a>
+                  </div>
                 <div class="tab-pane" id="BB">
-                  <p><a href="#">No idea where this one is...</a></p>
+                    <a href="http://www.markbryk.in/stuff/brickbreaker.zip">
+                        <div class="link-card link-card-left">
+                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/my/brickbreaker.png" />
+                        </div>
+                        <div class="link-card link-card-right">
+                            <h3>My First Program</h3>
+                            <p>This was the final project for my first Programming Class, CS102.</p>
+                            <p>We needed to build the Brickbreaker game using python.</p>
+                            <p>Feel free to download and compile.</p>
+                        </div>
+                      </a>
                 </div>
               </div>
 </div>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,8 +3,9 @@
                             One Man Show
                         </h1>
 <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/my/shelf.png" class="shelf">
-<div id="container">
     <hr class="style-five" style="margin-right: 20px"> 
+
+<div id="container">
     <a class="shelf-back" href="engineer" title="Engineer">
         <div class="notes" id="engineer"></div>
     </a>
@@ -29,36 +30,11 @@
     <a class="shelf-front" href="job" title="Student">
         <div class="notes" id="student"></div>
     </a>
-    <!--
-    <div class="notes notes-back" id="engineer">
-        <h1><a href="engineer">Engineer</a></h1>
-    </div>
-    <div class="notes notes-back"  id="programmer">
-        <h1><a href="programmer">Programmer</a></h1>
-    </div>
-    <div class="notes notes-back"  id="music">
-        <h1><a href="music">Musician</a></h1>
-    </div>
-    <div class="notes notes-back"  id="stoner">
-        <h1><a href="stone">Stoner</a></h1>
-    </div>
-    <div class="notes"  id="thinker">
-        <h1><a href="http://think.markbryk.in">Thinker</a></h1>
-    </div>                        
-    <div class="notes"  id="athlete">
-        <h1><a href="play">Athlete</a></h1>
-    </div>
-    <div class="notes" id="artist">
-        <h1><a href="artist">Artist</a></h1>
-    </div>                        
-    <div class="notes" id="student">
-        <h1><a href="job">Resume</a></h1>
-    </div>  
-    -->
+    
 </div>
 
 <div class="cleared"></div>
 
 <hr class="style-five" style="margin: 0 20px 25px 0">
 
-<a href="http://solosports.markbryk.in/"><button class="btn btn-inverse btn-large more-button"><h1 class="more-txt">And MORE!!!!</h1></button></a>
+<a href="http://solosports.markbryk.in/" title="SoloSports"><button class="btn btn-inverse btn-large more-button"><h1 class="more-txt">And MORE!!!!</h1></button></a>
